3019e80bc587e09980c339dd81d335af08b4f97f
bvasilop <bvasilop@gmail.com>
2019-05-08 11:46:40 -0700

    added product categories, rows and tables

--- src/components/ReviewCart.js
+++ src/components/ReviewCart.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import Cart from './Cart';
 import '../styles/index.scss';
 import reviewCartStyles from './reviewCart.module.scss';
 
 const ReviewCart = (props) => (
     <div className={reviewCartStyles.content}>
         <div className={reviewCartStyles.listHeader}>
-            <div className="showForMobile">{props.title}</div>
-            <div className="showForDesktop">{props.subtitle1}</div>
-            <div className="showForDesktop">{props.subtitle2}</div>
+            <div>{props.title}</div>
+            <div>{props.subtitle1}</div>
+            <div>{props.subtitle2}</div>
+            <div>{props.subtitle3}</div>
         </div>
+        <Cart/>
+        <div className={reviewCartStyles.listHeader}></div>
         <div className={reviewCartStyles.button}>
             <a href="/billing">
                 Checkout</a>
@@ -18,8 +22,9 @@ const ReviewCart = (props) => (
 );
 ReviewCart.defaultProps = { // using default props
     title: 'Your Cart',
-    subtitle1: 'Quantity',
-    subtitle2: 'Price'
+    subtitle1: 'Sku',
+    subtitle2: 'Quantity',
+    subtitle3: 'Price'
 };
 
 export default ReviewCart;
