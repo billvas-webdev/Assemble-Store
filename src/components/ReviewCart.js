c5ed2a4ecccc2315d1b8721bba9b2892a333d4a5
bvasilop <bvasilop@gmail.com>
2019-05-08 13:48:08 -0700

    corrected  rows formating for productTable

--- src/components/ReviewCart.js
+++ src/components/ReviewCart.js
@@ -7,10 +7,6 @@ const ReviewCart = (props) => (
     <div className={reviewCartStyles.content}>
         <div className={reviewCartStyles.listHeader}>
             <div>{props.title}</div>
-            <div>{props.subtitle1}</div>
-            <div>{props.subtitle2}</div>
-            <div>{props.subtitle3}</div>
-            <div>{props.subtitle4}</div>
         </div>
         <Cart/>
         <div className={reviewCartStyles.button}>
@@ -22,10 +18,6 @@ const ReviewCart = (props) => (
 );
 ReviewCart.defaultProps = { // using default props
     title: 'Your Cart',
-    subtitle1: 'Item',
-    subtitle2: 'Sku',
-    subtitle3: 'Quantity',
-    subtitle4: 'Price'
 };
 
 export default ReviewCart;
