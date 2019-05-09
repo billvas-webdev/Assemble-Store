73b995658a57fd89e21aae684998f48b12f1c9b2
bvasilop <bvasilop@gmail.com>
2019-05-09 14:18:33 -0700

    added grid layout for payment and shipping info components

--- src/components/Billing.js
+++ src/components/Billing.js
@@ -3,12 +3,68 @@ import '../styles/index.scss';
 import billingStyles from './billing.module.scss';
 
 const Billing = (props) => (
-    <div className={billingStyles.content}>
+    <div>
         <div className={billingStyles.listHeader}>
             <div className="showForMobile">{props.title}</div>
-            <div className="showForDesktop">{props.subtitle1}</div>
-            <div className="showForDesktop">{props.subtitle2}</div>
         </div>
+        <div className={billingStyles.listHeader}>
+            <form className={billingStyles.payment}>
+            <h2> Payment Information </h2>
+                <label>
+                    Name on Card
+                    <input type="text" name="input" placeholder="Name on Card"/>
+                </label>
+
+                <label>
+                    Card Number
+                    <input type="text" name="input" placeholder="XXXX XXXX XXXX XXXX"/>
+                </label>
+
+                <label>
+                    Expiration Date
+                    <input type="month" id="start" name="start" placeholder=""/>
+                </label>
+
+                <label>
+                    CVV
+                    <input type="text" name="input" placeholder="XXX"/>
+                </label>
+
+            </form>
+
+            <form className={billingStyles.shipment}>
+            <h2>Shipping Address</h2>
+                <label>
+                    Name
+                    <input type="text" name="input" placeholder="Full Name"/>
+                </label>
+
+                <label>
+                    Address
+                    <input type="text" name="input" placeholder="Address"/>
+                </label>
+
+                <label>
+                    Apt/suite/etc
+                    <input type="text" name="input" placeholder="Apt/suite/etc"/>
+                </label>
+
+                <label>
+                    City
+                    <input type="text" name="input" placeholder="City"/>
+                </label>
+                <label>
+                    State
+                    <input type="text" name="input" placeholder="State"/>
+                </label>
+                <label>
+                    Zip Code
+                    <input type="text" name="input" placeholder="Zip Code"/>
+                </label>
+
+            </form>
+        </div>
+
         <div className={billingStyles.button}>
             <a href="/orderComplete">
                 Place Order</a>
@@ -17,7 +73,9 @@ const Billing = (props) => (
 
 );
 Billing.defaultProps = { // using default props
-    title: 'Check Out'
+    title: 'Check Out',
+    subtitle1: 'Payment Information',
+    subtitle2: 'Shipping Information'
 };
 
 export default Billing;
